5c5bf995f24a18be24592aa5a4907fb5f366fe8e
Yanko Aleksandrov <superactro@gmail.com>
2020-09-22 11:23:28 +0300

    stuff

---
+++ 012_hands-on/07_hands-on/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type menu []restaurant
+
+type restaurant struct {
+	Name  string
+	Meals []meal
+}
+
+type meal struct {
+	Name  string
+	Items []menuItem
+}
+
+type menuItem struct {
+	Name  string
+	Price int
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("t.gohtml"))
+}
+
+func main() {
+
+	m := menu{
+		{
+			Name: "Maharaja",
+			Meals: []meal{
+				meal{
+					Name: "Breakfast",
+					Items: []menuItem{
+						menuItem{
+							"Bread", 10,
+						},
+						menuItem{
+							"Eggs", 20,
+						},
+					},
+				},
+				meal{
+					Name: "Lunch",
+					Items: []menuItem{
+						menuItem{
+							"Kur", 5,
+						},
+						menuItem{
+							"Hui", 15,
+						},
+					},
+				},
+				meal{
+					Name: "Dinner",
+					Items: []menuItem{
+						menuItem{
+							"Bum", 1,
+						},
+						menuItem{
+							"Bam", 2,
+						},
+					},
+				},
+			},
+		}, {
+			Name: "Maharani",
+			Meals: []meal{
+				meal{
+					Name: "Breakfast",
+					Items: []menuItem{
+						menuItem{
+							"Bread", 10,
+						},
+						menuItem{
+							"Eggs", 20,
+						},
+					},
+				},
+				meal{
+					Name: "Lunch",
+					Items: []menuItem{
+						menuItem{
+							"Kur", 5,
+						},
+						menuItem{
+							"Hui", 15,
+						},
+					},
+				},
+				meal{
+					Name: "Dinner",
+					Items: []menuItem{
+						menuItem{
+							"Bum", 1,
+						},
+						menuItem{
+							"Bam", 2,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := tpl.Execute(os.Stdout, m)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
